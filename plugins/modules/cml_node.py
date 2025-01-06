1c20c73166313b7533fd8157f3604dd97cf69d02
Jakub Trávník <jakub.travnik@gmail.com>
2025-01-06 18:01:50 +0100

    Revert "test populate_interfaces"
    This reverts commit ba442e3f3c636c3612b4d8677ad916447e1414ab.

--- plugins/modules/cml_node.py
+++ plugins/modules/cml_node.py
@@ -67,12 +67,6 @@ options:
         required: false
         type: str
 
-    populate_interfaces:
-        description: Automatically create a pre-defined number of interfaces on node creation.
-        required: false
-        type: bool
-        default: false
-
     x:
         description: X coordinate on topology canvas
         required: false
@@ -135,7 +129,6 @@ def run_module():
         image_definition=dict(type='str'),
         config=dict(type='str'),
         tags=dict(type='list', elements='str'),
-        populate_interfaces=dict(type='bool', default=False),
         x=dict(type='int'),
         y=dict(type='int'),
         wait=dict(type='bool', default=False),
@@ -162,7 +155,7 @@ def run_module():
         if node is None:
             if module.check_mode:
                 module.exit_json(changed=True)
-            node = lab.create_node(label=cml.params['name'], node_definition=cml.params['node_definition'], populate_interfaces=cml.params['populate_interfaces'])
+            node = lab.create_node(label=cml.params['name'], node_definition=cml.params['node_definition'])
             cml.result['changed'] = True
     elif cml.params['state'] == 'started':
         if node is None:
